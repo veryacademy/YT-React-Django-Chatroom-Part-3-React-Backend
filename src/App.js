cb43837da6fac3c1692bf3c8a664d5f04fa247cb
Khalid El Fasely <76157761+khalidfasely@users.noreply.github.com>
2023-03-12 13:52:49 +0100

    Update App.js
    Update App.js to a functional component with TypeScript.

--- src/App.js
+++ src/App.js
@@ -1,198 +1,152 @@
-import React, { Component } from 'react';
-import { w3cwebsocket as W3CWebSocket } from "websocket";
+import React, { useEffect, useMemo, useState } from 'react';
+import { w3cwebsocket as W3CWebSocket } from 'websocket';
 
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import Paper from '@material-ui/core/Paper';
-import Avatar from '@material-ui/core/Avatar';
+import { CssBaseline, Button, TextField, Link, Grid, Typography, Container, Card, CardHeader, Paper, Avatar } from '@mui/material';
 
-import { withStyles } from "@material-ui/core/styles";
-
-const useStyles = theme => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-  root: {
-    boxShadow: 'none',
-  }
-});
-
-class App extends Component {
-
-  state = {
-    isLoggedIn: false,
-    messages: [],
-    value: '',
-    name: '',
-    room: 'vacad',
-  }
-
-  client = new W3CWebSocket('ws://django-chat-app.herokuapp.com/ws/chat/' + this.state.room + '/');
-
-  onButtonClicked = (e) => {
-    this.client.send(JSON.stringify({
-      type: "message",
-      message: this.state.value,
-      name: this.state.name
-    }));
-    this.state.value = ''
-    e.preventDefault();
-  }
-
-  componentDidMount() {
-    this.client.onopen = () => {
-      console.log('WebSocket Client Connected');
-    };
-    this.client.onmessage = (message) => {
-      const dataFromServer = JSON.parse(message.data);
-      console.log('got reply! ', dataFromServer.type);
-      if (dataFromServer) {
-        this.setState((state) =>
-          ({
-            messages: [...state.messages,
-            {
-              msg: dataFromServer.message,
-              name: dataFromServer.name,
-            }]
-          })
-        );
-      }
-    };
-  }
-
-  render() {
-    const { classes } = this.props;
-    return (
-      <Container component="main" maxWidth="xs">
-        {this.state.isLoggedIn ?
-          <div style={{ marginTop: 50, }}>
-            Room Name: {this.state.room}
-            <Paper style={{ height: 500, maxHeight: 500, overflow: 'auto', boxShadow: 'none', }}>
-              {this.state.messages.map(message => <>
-                <Card className={classes.root}>
-                  <CardHeader
-                    avatar={
-                      <Avatar className={classes.avatar}>
-                        R
-                  </Avatar>
-                    }
-                    title={message.name}
-                    subheader={message.msg}
-                  />
-                </Card>
-              </>)}
-            </Paper>
-
-            <form className={classes.form} noValidate onSubmit={this.onButtonClicked}>
-              <TextField
-                id="outlined-helperText"
-                label="Make a comment"
-                defaultValue="Default Value"
-                variant="outlined"
-                value={this.state.value}
-                fullWidth
-                onChange={e => {
-                  this.setState({ value: e.target.value });
-                  this.value = this.state.value;
-                }}
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-                className={classes.submit}
-              >
-                Start Chatting
-                </Button>
-            </form>
-          </div>
-          :
-          <div>
-            <CssBaseline />
-            <div className={classes.paper}>
-              <Typography component="h1" variant="h5">
-                ChattyRooms
-                </Typography>
-              <form className={classes.form} noValidate onSubmit={value => this.setState({ isLoggedIn: true })}>
-                <TextField
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="email"
-                  label="Chatroom Name"
-                  name="Chatroom Name"
-                  autoFocus
-                  value={this.state.room}
-                  onChange={e => {
-                    this.setState({ room: e.target.value });
-                    this.value = this.state.room;
-                  }}
-                />
-                <TextField
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="Username"
-                  label="Username"
-                  type="Username"
-                  id="Username"
-                  value={this.state.name}
-                  onChange={e => {
-                    this.setState({ name: e.target.value });
-                    this.value = this.state.name;
-                  }}
-                />
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="primary"
-                  className={classes.submit}
-                >
-                  Start Chatting
-                  </Button>
-                <Grid container>
-                  <Grid item xs>
-                    <Link href="#" variant="body2">
-                      Forgot password?
-                      </Link>
-                  </Grid>
-                  <Grid item>
-                    <Link href="#" variant="body2">
-                      {"Don't have an account? Sign Up"}
-                    </Link>
-                  </Grid>
-                </Grid>
-              </form>
-            </div>
-          </div>}
-      </Container>
-    )
-
-  }
+interface Message {
+  name: string,
+  msg: string
 }
-export default withStyles(useStyles)(App)
+
+const App: React.FC = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+    const [messages, setMessages] = useState<Message[]>([]);
+    const [value, setValue] = useState<string>('');
+    const [name, setName] = useState<string>('');
+    const [room, setRoom] = useState<string>('vacad');
+
+    //to not recreate every time the component rerendered
+    const client = useMemo(() => {
+        return new W3CWebSocket('ws://127.0.0.1:8000/ws/chat/' + room + '/');
+    }, [])
+
+    const onButtonClicked = (e: any) => {
+        client.send(JSON.stringify({
+            type: 'message',
+            message: value,
+            name: name,
+        }));
+        setValue('');
+        e.preventDefault();
+    }
+
+    useEffect(() => {
+        client.onopen = () => {
+            console.log("websocket client connected");
+          }
+          client.onmessage = (message: any) => {
+            const dataFromServer = JSON.parse(message.data);
+            console.log('got reply! from:', dataFromServer.name);
+            if (dataFromServer) {
+                setMessages((prev: Message[]) => [...prev,
+                    {
+                        msg: dataFromServer.message,
+                        name: dataFromServer.name,
+                    }
+                ])
+            }
+          }
+    }, [])
+
+    return (
+        <Container component='main' maxWidth='xs'>
+            {isLoggedIn ? (
+                <div style={{ marginTop: 50, }}>
+                    Room Name: {room}
+                    <Paper style={{ height: 500, maxHeight: 500, overflow: 'auto', boxShadow: 'none', }}>
+                        {messages.map((message: Message, idx) => <div key={idx}>
+                            <Card>
+                                <CardHeader
+                                    avatar={
+                                    <Avatar>
+                                        {message.name[0].toUpperCase()}
+                                </Avatar>
+                                    }
+                                    title={message.name}
+                                    subheader={message.msg}
+                                />
+                                </Card>
+                        </div>)}
+                    </Paper>
+
+                <form noValidate onSubmit={onButtonClicked}>
+                    <TextField
+                        id="outlined-helperText"
+                        label="Make a comment"
+                        //defaultValue="Default Value"
+                        variant="outlined"
+                        value={value}
+                        fullWidth
+                        onChange={e => setValue(e.target.value)}
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                    >
+                        Start Chatting
+                    </Button>
+                </form>
+            </div>
+            ) : (
+            <div>
+                <CssBaseline />
+                <div>
+                    <Typography component="h1" variant="h5">
+                        ChattyRooms
+                    </Typography>
+                    <form noValidate onSubmit={value => setIsLoggedIn(true)}>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Chatroom Name"
+                            name="Chatroom Name"
+                            autoFocus
+                            value={room}
+                            onChange={e => setRoom(e.target.value)}
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="Username"
+                            label="Username"
+                            type="Username"
+                            id="Username"
+                            value={name}
+                            onChange={e => setName(e.target.value)}
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                        >
+                            Start Chatting
+                        </Button>
+                        <Grid container>
+                        <Grid item xs>
+                            <Link href="#" variant="body2">
+                                Forgot password?
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href="#" variant="body2">
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                        </Grid>
+                        </Grid>
+                    </form>
+                </div>
+            </div>
+            )}
+        </Container>
+    );
+}
+
+export default App;
